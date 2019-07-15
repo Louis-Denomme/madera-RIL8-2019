72dc6f664c7354b1cba58efc13bbb25af477c890
louis.D <louis.denomme.pro@gmail.com>
2019-07-15 19:20:59 +0200

    WIP accueil

--- application/views/vAccueil.php
+++ application/views/vAccueil.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Accueil</title>
+    <!-- Balises obligatoires dans chaque vue-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/normalize.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans&display=swap" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet">
+
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/interface/Interface.css">
 </head>
 <body>
-    <?php
-    include  'interface/vHeader.php';
-    include 'interface/vNavigation.php';
-    ?>
+    <div> <?php require  'interface/vHeader.php'; ?></div>
+    <div id="myBody">
+        <?php require 'interface/vNavigation.php'; ?>
+        <div id="corps">
 
-<a href='<?php echo base_url() . "index.php/Connexion/deconnexion"; ?>'>Déconnexion</a>
-
+        </div>
+    </div>
 </body>
 </html>
