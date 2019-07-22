c7d69460b8a6b152a584bd420720076b2875a224
louis.D <louis.denomme.pro@gmail.com>
2019-07-22 19:54:33 +0200

    wip

---
+++ application/models/mExteriorFinish.php
@@ -0,0 +1,45 @@
+<?php
+
+
+class mExteriorFinish
+{
+    private $id;
+    private $name;
+
+    //<editor-fold desc="Getters and setters">
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    //</editor-fold>
+
+
+}
---
+++ application/models/mSupplier.php
@@ -0,0 +1,128 @@
+<?php
+
+
+class mSupplier
+{
+    private $id;
+    private $idAdress;
+    private $phone;
+    private $mail;
+    private $name;
+    private $createAt;
+    private $updatedAt;
+
+    //<editor-fold desc="Getters and setters">
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdAdress()
+    {
+        return $this->idAdress;
+    }
+
+    /**
+     * @param mixed $idAdress
+     */
+    public function setIdAdress($idAdress)
+    {
+        $this->idAdress = $idAdress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @param mixed $mail
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreateAt()
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * @param mixed $createAt
+     */
+    public function setCreateAt($createAt)
+    {
+        $this->createAt = $createAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param mixed $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+    //</editor-fold>
+
+}
